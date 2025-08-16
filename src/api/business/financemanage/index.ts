dc8dab3fabdfd72a4a76ee39fe537ed96e2d8d24
450MileForHome <741144861@qq.com>
2025-08-16 13:18:32 +0800

    #11 工控通-融资租赁业务-租后管理

---
+++ src/api/business/financemanage/index.ts
@@ -0,0 +1,122 @@
+import request from '@/config/axios'
+
+// 融资租赁租后管理 VO
+export interface FinanceManageVO {
+  id: number // 融资租赁租后管理表单主键
+  leaseId: number // 融资租赁单编号
+  companyId: number // 企业id
+  leaseAmount: number // 租赁总额
+  leaseAmountSurplus: number // 租赁余额
+  userId: number // 检查人的用户编号
+  manageDate: Date // 检查日期
+  corpNameChanged: boolean // 企业名称是否发生变更
+  legalRepChanged: boolean // 法人代表是否发生变更
+  actualControllerChanged: boolean // 实际控制人是否发生变更
+  mgmtTeamChanged: boolean // 企业主要管理层是否发生变动
+  regCapitalChanged: boolean // 企业注册资本是否发生变化
+  licenseAnnualOk: boolean // 企业营业执照是否通过年检
+  bizAddrChanged: boolean // 企业经营场所是否发生变化
+  equityStructChanged: boolean // 企业股权结构是否发生变化
+  fundCircleInRelated: boolean // 资金是否存在在关联方流转
+  industryChanged: boolean // 所在行业是否发生变化
+  mainBizChanged: boolean // 主营业务和主要产品是否发生变化
+  keyCustomersChanged: boolean // 主要客户是否发生变化
+  upstreamChanged: boolean // 上游企业是否发生变化
+  processTechChanged: boolean // 生产工艺及技术是否发生变化
+  utilityTaxDecline: boolean // 生产用电量、用水量及纳税数额是否显著下降
+  startupNormal: boolean // 近期开工情况是否正常
+  officeDisorder: boolean // 办公秩序是否混乱，人员稀少
+  newProjectLaunched: boolean // 是否实施新项目
+  newBadCredit: boolean // 企业是否有新增征信不良记录
+  involvedLawsuit: boolean // 企业是否涉及司法诉讼
+  involvedPenalty: boolean // 企业是否涉及行政处罚
+  taxPaymentNormal: boolean // 企业是否正常缴纳税金
+  salaryPaidOnTime: boolean // 企业是否按期支付员工工资
+  cooperatePostLoan: boolean // 企业是否配合贷后管理工作
+  leaseInPlace: boolean // 租赁物件是否在约定使用场所
+  leaseNormal: boolean // 租赁物件情况是否正常
+  otherDescriptions: string // 其他检查情况说明
+  totalAssets: number // 总资产
+  totalAssetsRemark: string // 总资产备注
+  inventory: number // 存货
+  inventoryRemark: string // 存货备注
+  accountsReceivable: number // 应收账款
+  accountsReceivableRemark: string // 应收账款备注
+  totalLiabilities: number // 总负债
+  totalLiabilitiesRemark: string // 总负债备注
+  bankLoans: number // 银行借款
+  bankLoansRemark: string // 银行借款备注
+  accountsPayable: number // 应付账款
+  accountsPayableRemark: string // 应付账款备注
+  mainBizIncome: number // 主营业务收入
+  mainBizIncomeRemark: string // 主营业务收入备注
+  netProfit: number // 净利润
+  netProfitRemark: string // 净利润备注
+  debtAssetRatio: number // 资产负债率(%)
+  debtAssetRatioRemark: string // 资产负债率(%)备注
+  fsObviouslyFalse: boolean // 财务报表是否明显不实
+  fsObviouslyFalseReason: string // 财务报表是否明显不实原因
+  arSharpRiseDelay: boolean // 应收账款有无大幅增加，收回拖延
+  arSharpRiseDelayReason: string // 应收账款有无大幅增加，收回拖延原因
+  inventorySurge: boolean // 存货是否较大，有无突然增加
+  inventorySurgeReason: string // 存货是否较大，有无突然增加原因
+  bankDebtRise: boolean // 银行债务有无大量增加
+  bankDebtRiseReason: string // 银行债务有无大量增加原因
+  debtStructChange: boolean // 资产负债结构有无重大变化
+  debtStructChangeReason: string // 资产负债结构有无重大变化原因
+  currentRatioLow: boolean // 流动比远低于正常值
+  currentRatioLowReason: string // 流动比远低于正常值原因
+  costUpProfitDown: boolean // 成本上升或利润下降
+  costUpProfitDownReason: string // 成本上升或利润下降原因
+  cashBalanceDecline: boolean // 货币资金余额是否不断下降
+  cashBalanceDeclineReason: string // 货币资金余额是否不断下降原因
+  paymentDelay: boolean // 是否拖延支付用款费用
+  paymentDelayReason: string // 是否拖延支付用款费用原因
+  financialSituation: string // 财务其他风险
+  overallRiskEval: string // 对项目风险状况的总体评价
+  overallLeaseEval: string // 对租赁物件状况的总体评价
+  riskMgmtSuggestion: number // 对该项目的风险管理策略建议
+  filePath: string // 文件路径
+  status: number // 单据状态
+  processInstanceId: string // 流程实例的编号
+  deptId: number // 部门id
+}
+
+// 融资租赁租后管理 API
+export const FinanceManageApi = {
+  // 查询融资租赁租后管理分页
+  getFinanceManagePage: async (params: any) => {
+    return await request.get({ url: `/business/finance-manage/page`, params })
+  },
+
+  // 查询融资租赁租后管理详情
+  getFinanceManage: async (id: number) => {
+    return await request.get({ url: `/business/finance-manage/get?id=` + id })
+  },
+
+  // 新增融资租赁租后管理
+  createFinanceManage: async (data: FinanceManageVO) => {
+    return await request.post({ url: `/business/finance-manage/create`, data })
+  },
+
+  // 修改融资租赁租后管理
+  updateFinanceManage: async (data: FinanceManageVO) => {
+    return await request.put({ url: `/business/finance-manage/update`, data })
+  },
+
+  // 删除融资租赁租后管理
+  deleteFinanceManage: async (id: number) => {
+    return await request.delete({ url: `/business/finance-manage/delete?id=` + id })
+  },
+
+  // 导出融资租赁租后管理 Excel
+  exportFinanceManage: async (params) => {
+    return await request.download({ url: `/business/finance-manage/export-excel`, params })
+  },
+
+
+  // 融资租赁租后管理送审
+  sendApprove: async (id: number) => {
+    return await request.post({ url: `/business/finance-manage/sendApprove?id=` + id })
+  }
+}
---
+++ src/views/business/financemanage/index.vue
@@ -0,0 +1,321 @@
+<template>
+  <ContentWrap>
+    <!-- 搜索工作栏 -->
+    <el-form
+      class="-mb-15px"
+      :model="queryParams"
+      ref="queryFormRef"
+      :inline="true"
+      label-width="68px"
+    >
+      <el-form-item label="融资租赁单编号" prop="leaseId">
+        <el-input
+          v-model="queryParams.leaseId"
+          placeholder="请输入融资租赁单编号"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item label="企业id" prop="companyId">
+        <el-select
+          v-model="queryParams.companyId"
+          placeholder="请选择企业id"
+          clearable
+          class="!w-240px"
+        >
+          <el-option label="请选择字典生成" value="" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="检查日期" prop="manageDate">
+        <el-date-picker
+          v-model="queryParams.manageDate"
+          value-format="YYYY-MM-DD HH:mm:ss"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :default-time="[new Date('1 00:00:00'), new Date('1 23:59:59')]"
+          class="!w-220px"
+        />
+      </el-form-item>
+      <el-form-item label="单据状态" prop="status">
+        <el-select
+          v-model="queryParams.status"
+          placeholder="请选择单据状态"
+          clearable
+          class="!w-240px"
+        >
+          <el-option label="请选择字典生成" value="" />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="handleQuery"><Icon icon="ep:search" class="mr-5px" /> 搜索</el-button>
+        <el-button @click="resetQuery"><Icon icon="ep:refresh" class="mr-5px" /> 重置</el-button>
+        <el-button
+          type="primary"
+          plain
+          @click="openForm('create')"
+          v-hasPermi="['business:finance-manage:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" /> 新增
+        </el-button>
+        <el-button
+          type="success"
+          plain
+          @click="handleExport"
+          :loading="exportLoading"
+          v-hasPermi="['business:finance-manage:export']"
+        >
+          <Icon icon="ep:download" class="mr-5px" /> 导出Excel
+        </el-button>
+        <el-button
+          type="success"
+          plain
+          @click="handleExport"
+          :loading="exportLoading"
+          v-hasPermi="['business:finance-manage:export']"
+        >
+          <Icon icon="ep:download" class="mr-5px" /> 导出检查登记表
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </ContentWrap>
+
+  <!-- 列表 -->
+  <ContentWrap>
+    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
+      <el-table-column label="编号" align="center" prop="id" />
+      <el-table-column label="企业id" align="center" prop="companyId" />
+      <el-table-column label="租赁总额" align="center" prop="leaseAmount" />
+      <el-table-column label="租赁余额" align="center" prop="leaseAmountSurplus" />
+      <el-table-column label="检查人的用户编号" align="center" prop="userId" />
+      <el-table-column
+        label="检查日期"
+        align="center"
+        prop="manageDate"
+        :formatter="dateFormatter"
+        width="180px"
+      />
+      <el-table-column label="单据状态" align="center" prop="status" />
+      <el-table-column label="操作" align="center" min-width="120px">
+        <template #default="scope">
+          <el-button
+            link
+            type="primary"
+            @click="openForm('update', scope.row.id)"
+            v-hasPermi="['business:finance-manage:update']"
+          >
+            编辑
+          </el-button>
+          <el-button
+            link
+            type="primary"
+            @click="sendApprove(scope.row.id)"
+            v-hasPermi="['business:finance-manage:sendApprove']"
+          >
+            送审
+          </el-button>
+          <el-button
+            link
+            type="danger"
+            @click="handleDelete(scope.row.id)"
+            v-hasPermi="['business:finance-manage:delete']"
+          >
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <Pagination
+      :total="total"
+      v-model:page="queryParams.pageNo"
+      v-model:limit="queryParams.pageSize"
+      @pagination="getList"
+    />
+  </ContentWrap>
+
+  <!-- 表单弹窗：添加/修改 -->
+  <FinanceManageForm ref="formRef" @success="getList" />
+</template>
+
+<script setup lang="ts">
+import { getBoolDictOptions, DICT_TYPE } from '@/utils/dict'
+import { dateFormatter } from '@/utils/formatTime'
+import download from '@/utils/download'
+import { FinanceManageApi, FinanceManageVO } from '@/api/business/financemanage'
+import FinanceManageForm from './FinanceManageForm.vue'
+import {FinanceLeaseApi} from "@/api/business/financelease";
+import {FinanceApplicationApi} from "@/api/business/financeapplication";
+
+/** 融资租赁租后管理 列表 */
+defineOptions({ name: 'FinanceManage' })
+
+const message = useMessage() // 消息弹窗
+const { t } = useI18n() // 国际化
+
+const loading = ref(true) // 列表的加载中
+const list = ref<FinanceManageVO[]>([]) // 列表的数据
+const total = ref(0) // 列表的总页数
+const queryParams = reactive({
+  pageNo: 1,
+  pageSize: 10,
+  leaseId: undefined,
+  companyId: undefined,
+  leaseAmount: undefined,
+  leaseAmountSurplus: undefined,
+  manageDate: [],
+  corpNameChanged: undefined,
+  legalRepChanged: undefined,
+  actualControllerChanged: undefined,
+  mgmtTeamChanged: undefined,
+  regCapitalChanged: undefined,
+  licenseAnnualOk: undefined,
+  bizAddrChanged: undefined,
+  equityStructChanged: undefined,
+  fundCircleInRelated: undefined,
+  industryChanged: undefined,
+  mainBizChanged: undefined,
+  keyCustomersChanged: undefined,
+  upstreamChanged: undefined,
+  processTechChanged: undefined,
+  utilityTaxDecline: undefined,
+  startupNormal: undefined,
+  officeDisorder: undefined,
+  newProjectLaunched: undefined,
+  newBadCredit: undefined,
+  involvedLawsuit: undefined,
+  involvedPenalty: undefined,
+  taxPaymentNormal: undefined,
+  salaryPaidOnTime: [],
+  cooperatePostLoan: undefined,
+  leaseInPlace: undefined,
+  leaseNormal: undefined,
+  otherDescriptions: undefined,
+  totalAssets: undefined,
+  totalAssetsRemark: undefined,
+  inventory: undefined,
+  inventoryRemark: undefined,
+  accountsReceivable: undefined,
+  accountsReceivableRemark: undefined,
+  totalLiabilities: undefined,
+  totalLiabilitiesRemark: undefined,
+  bankLoans: undefined,
+  bankLoansRemark: undefined,
+  accountsPayable: undefined,
+  accountsPayableRemark: undefined,
+  mainBizIncome: undefined,
+  mainBizIncomeRemark: undefined,
+  netProfit: undefined,
+  netProfitRemark: undefined,
+  debtAssetRatio: undefined,
+  debtAssetRatioRemark: undefined,
+  fsObviouslyFalse: undefined,
+  fsObviouslyFalseReason: undefined,
+  arSharpRiseDelay: undefined,
+  arSharpRiseDelayReason: undefined,
+  inventorySurge: undefined,
+  inventorySurgeReason: undefined,
+  bankDebtRise: undefined,
+  bankDebtRiseReason: undefined,
+  debtStructChange: undefined,
+  debtStructChangeReason: undefined,
+  currentRatioLow: undefined,
+  currentRatioLowReason: undefined,
+  costUpProfitDown: undefined,
+  costUpProfitDownReason: undefined,
+  cashBalanceDecline: undefined,
+  cashBalanceDeclineReason: undefined,
+  paymentDelay: undefined,
+  paymentDelayReason: undefined,
+  financialSituation: undefined,
+  overallRiskEval: undefined,
+  overallLeaseEval: undefined,
+  riskMgmtSuggestion: undefined,
+  status: undefined,
+  processInstanceId: undefined,
+  createTime: [],
+  deptId: undefined,
+})
+const queryFormRef = ref() // 搜索的表单
+const exportLoading = ref(false) // 导出的加载中
+
+/** 查询列表 */
+const getList = async () => {
+  loading.value = true
+  try {
+    const data = await FinanceManageApi.getFinanceManagePage(queryParams)
+    list.value = data.list
+    total.value = data.total
+  } finally {
+    loading.value = false
+  }
+}
+
+/** 搜索按钮操作 */
+const handleQuery = () => {
+  queryParams.pageNo = 1
+  getList()
+}
+
+/** 重置按钮操作 */
+const resetQuery = () => {
+  queryFormRef.value.resetFields()
+  handleQuery()
+}
+
+/** 添加/修改操作 */
+const formRef = ref()
+const openForm = (type: string, id?: number) => {
+  formRef.value.open(type, id)
+}
+
+/** 删除按钮操作 */
+const handleDelete = async (id: number) => {
+  try {
+    // 删除的二次确认
+    await message.delConfirm()
+    // 发起删除
+    await FinanceManageApi.deleteFinanceManage(id)
+    message.success(t('common.delSuccess'))
+    // 刷新列表
+    await getList()
+  } catch {}
+}
+
+/** 导出按钮操作 */
+const handleExport = async () => {
+  try {
+    // 导出的二次确认
+    await message.exportConfirm()
+    // 发起导出
+    exportLoading.value = true
+    const data = await FinanceManageApi.exportFinanceManage(queryParams)
+    download.excel(data, '融资租赁租后管理.xls')
+  } catch {
+  } finally {
+    exportLoading.value = false
+  }
+}
+
+/** 送审按钮操作 */
+const sendApprove = async (id: number) => {
+  try {
+    // 送审的二次确认
+    await message.sendApproveConfirm()
+    // 发起删除
+    await FinanceApplicationApi.sendApprove(id)
+    message.success(t('common.sendApproveSuccess'))
+    // 刷新列表
+    await getList()
+  } catch {
+  } finally {
+
+  }
+}
+
+/** 初始化 **/
+onMounted(() => {
+  getList()
+})
+</script>
