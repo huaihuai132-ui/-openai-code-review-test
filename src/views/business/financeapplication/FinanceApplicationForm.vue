303d5c201d19d66f793ec8adcc51a49f0d9b7df0
周嘉琪 <1040810484@qq.com>
2025-08-18 16:00:29 +0800

    #8 工控通前端-立项管理新增修改页调整

--- src/views/business/financeapplication/FinanceApplicationForm.vue
+++ src/views/business/financeapplication/FinanceApplicationForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <Dialog :title="dialogTitle" v-model="dialogVisible">
+  <Dialog :title="dialogTitle" v-model="dialogVisible" class="right-full-dia">
     <el-form
       ref="formRef"
       :model="formData"
@@ -7,132 +7,170 @@
       label-width="100px"
       v-loading="formLoading"
     >
-      <el-form-item label="企业" prop="companyId">
-        <el-select v-model="formData.companyId" placeholder="请选择企业">
-          <el-option
-            v-for="item in companyList"
-            :key="item.id"
-            :label="item.enterpriseName"
-            :value="item.id"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="立项申请编码" prop="applicationCode">
-        <el-input v-model="formData.applicationCode" placeholder="请输入立项申请编码" />
-      </el-form-item>
-      <el-form-item label="标的名称" prop="leasedProperty">
-        <el-input v-model="formData.leasedProperty" placeholder="请输入标的名称" />
-      </el-form-item>
-      <el-form-item label="标的数量" prop="leasedPropertyNum">
-        <el-input v-model="formData.leasedPropertyNum" placeholder="请输入标的数量" />
-      </el-form-item>
-      <el-form-item label="拟标的净值" prop="leasedPropertyValue">
-        <el-input v-model="formData.leasedPropertyValue" placeholder="请输入拟标的净值" />
-      </el-form-item>
-      <el-form-item label="申请额度" prop="leaseAmount">
-        <el-input v-model="formData.leaseAmount" placeholder="请输入申请额度" />
-      </el-form-item>
-      <el-form-item label="承租租期" prop="leaseTerm">
-        <el-input v-model="formData.leaseTerm" placeholder="请输入承租租期" />
-      </el-form-item>
-      <el-form-item label="是否列为国家产业政策限制/淘汰的高能耗、高水耗、高污染企业	" prop="restrictedByPolicy" label-width="388px">
-        <el-radio-group v-model="formData.restrictedByPolicy">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="租赁物是否为行业先进技术设备	" prop="advancedTechEquip" label-width="388px">
-        <el-radio-group v-model="formData.advancedTechEquip">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="租赁物是否为濒临淘汰或折旧严重的设备" prop="obsoleteOrOverdep" label-width="388px">
-        <el-radio-group v-model="formData.obsoleteOrOverdep">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="是否涉嫌走私、偷逃税款或骗取出口退税" prop="taxFraudRisk" label-width="388px">
-        <el-radio-group v-model="formData.taxFraudRisk">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="企业（项目）立项、生产经营用地批文是否齐全" prop="landPermitOk" label-width="388px">
-        <el-radio-group v-model="formData.landPermitOk">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="环评、能耗、安全消防是否达标" prop="ehsCompliance" label-width="388px">
-        <el-radio-group v-model="formData.ehsCompliance">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="营业执照等证件是否经过当地年检" prop="licenseAnnualOk" label-width="388px">
-        <el-radio-group v-model="formData.licenseAnnualOk">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="申请人及其法定代表人是否有不良信用记录" prop="badCreditRecord" label-width="388px">
-        <el-radio-group v-model="formData.badCreditRecord">
-          <el-radio
-            v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
-            :key="dict.value"
-            :label="dict.value"
-          >
-            {{ dict.label }}
-          </el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="文件路径" prop="filePath">
-        <UploadFile
-          v-model="formData.filePath"
-          :limit="10"
-          :file-type="['jpg', 'png', 'pdf', 'doc', 'docx']"
-          :file-size="10"
-          :drag="true"
-          directory="uploads"
-        />
-      </el-form-item>
+      <!-- 上部分：输入框以一行三列形式排列 -->
+      <div class="form-section">
+        <h3 class="section-title">基本信息</h3>
+        <div class="form-row">
+          <el-form-item label="企业" prop="companyId">
+            <el-select v-model="formData.companyId" placeholder="请选择企业">
+              <el-option
+                v-for="item in companyList"
+                :key="item.id"
+                :label="item.enterpriseName"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="立项申请编码" prop="applicationCode">
+            <el-input v-model="formData.applicationCode" placeholder="请输入立项申请编码" />
+          </el-form-item>
+          <el-form-item label="标的名称" prop="leasedProperty">
+            <el-input v-model="formData.leasedProperty" placeholder="请输入标的名称" />
+          </el-form-item>
+        </div>
+        <div class="form-row">
+          <el-form-item label="标的数量" prop="leasedPropertyNum">
+            <el-input v-model="formData.leasedPropertyNum" placeholder="请输入标的数量" />
+          </el-form-item>
+          <el-form-item label="拟标的净值" prop="leasedPropertyValue">
+            <el-input v-model="formData.leasedPropertyValue" placeholder="请输入拟标的净值" />
+          </el-form-item>
+          <el-form-item label="申请额度" prop="leaseAmount">
+            <el-input v-model="formData.leaseAmount" placeholder="请输入申请额度" />
+          </el-form-item>
+        </div>
+        <div class="form-row">
+          <el-form-item label="承租租期" prop="leaseTerm">
+            <el-input v-model="formData.leaseTerm" placeholder="请输入承租租期" />
+          </el-form-item>
+          <el-form-item label="" prop="">
+            <div></div>
+          </el-form-item>
+          <el-form-item label="" prop="">
+            <div></div>
+          </el-form-item>
+        </div>
+      </div>
+
+      <!-- 下部分：Tab组件 -->
+      <div class="form-section">
+        <el-tabs v-model="activeTab" class="form-tabs">
+                    <el-tab-pane label="指标" name="radio">
+            <div class="radio-content">
+              <div class="radio-row">
+                <el-form-item label="是否列为国家产业政策限制/淘汰的高能耗、高水耗、高污染企业" prop="restrictedByPolicy" label-width="388px">
+                  <el-radio-group v-model="formData.restrictedByPolicy">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+                <el-form-item label="租赁物是否为行业先进技术设备" prop="advancedTechEquip" label-width="388px">
+                  <el-radio-group v-model="formData.advancedTechEquip">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+              </div>
+              <div class="radio-row">
+                <el-form-item label="租赁物是否为濒临淘汰或折旧严重的设备" prop="obsoleteOrOverdep" label-width="388px">
+                  <el-radio-group v-model="formData.obsoleteOrOverdep">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+                <el-form-item label="是否涉嫌走私、偷逃税款或骗取出口退税" prop="taxFraudRisk" label-width="388px">
+                  <el-radio-group v-model="formData.taxFraudRisk">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+              </div>
+              <div class="radio-row">
+                <el-form-item label="企业（项目）立项、生产经营用地批文是否齐全" prop="landPermitOk" label-width="388px">
+                  <el-radio-group v-model="formData.landPermitOk">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+                <el-form-item label="环评、能耗、安全消防是否达标" prop="ehsCompliance" label-width="388px">
+                  <el-radio-group v-model="formData.ehsCompliance">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+              </div>
+              <div class="radio-row">
+                <el-form-item label="营业执照等证件是否经过当地年检" prop="licenseAnnualOk" label-width="388px">
+                  <el-radio-group v-model="formData.licenseAnnualOk">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+                <el-form-item label="申请人及其法定代表人是否有不良信用记录" prop="badCreditRecord" label-width="388px">
+                  <el-radio-group v-model="formData.badCreditRecord">
+                    <el-radio
+                      v-for="dict in getBoolDictOptions(DICT_TYPE.INFRA_BOOLEAN_STRING)"
+                      :key="String(dict.value)"
+                      :label="dict.value"
+                    >
+                      {{ dict.label }}
+                    </el-radio>
+                  </el-radio-group>
+                </el-form-item>
+              </div>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="附件上传" name="upload">
+            <div class="upload-content">
+              <el-form-item label="文件路径" prop="filePath">
+                <UploadFile
+                  v-model="formData.filePath"
+                  :limit="10"
+                  file-type="jpg,png,pdf,doc,docx"
+                  :file-size="10"
+                  :drag="true"
+                  directory="uploads"
+                />
+              </el-form-item>
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
     </el-form>
     <template #footer>
       <el-button @click="submitForm" type="primary" :disabled="formLoading">确 定</el-button>
@@ -187,8 +225,8 @@ const formRules = reactive({
   leasedPropertyValue: [{ required: true, message: '拟标的净值不能为空', trigger: 'blur' }],
   leaseAmount: [{ required: true, message: '申请额度不能为空', trigger: 'blur' }],
   leaseTerm: [{ required: true, message: '承租租期不能为空', trigger: 'blur' }],
-  restrictedByPolicy: [{ required: true, message: '是否列为国家产业政策限制/淘汰的高能耗、高水耗、高污染企业	不能为空', trigger: 'blur' }],
-  advancedTechEquip: [{ required: true, message: '租赁物是否为行业先进技术设备	不能为空', trigger: 'blur' }],
+  restrictedByPolicy: [{ required: true, message: '是否列为国家产业政策限制/淘汰的高能耗、高水耗、高污染企业不能为空', trigger: 'blur' }],
+  advancedTechEquip: [{ required: true, message: '租赁物是否为行业先进技术设备不能为空', trigger: 'blur' }],
   obsoleteOrOverdep: [{ required: true, message: '租赁物是否为濒临淘汰或折旧严重的设备不能为空', trigger: 'blur' }],
   taxFraudRisk: [{ required: true, message: '是否涉嫌走私、偷逃税款或骗取出口退税不能为空', trigger: 'blur' }],
   landPermitOk: [{ required: true, message: '企业（项目）立项、生产经营用地批文是否齐全不能为空', trigger: 'blur' }],
@@ -199,6 +237,8 @@ const formRules = reactive({
 })
 const formRef = ref() // 表单 Ref
 const companyList = ref<FinanceCompanyVO[]>([]) // 公司列表
+const activeTab = ref('radio') // 当前激活的tab
+
 
 /** 打开弹窗 */
 const open = async (type: string, id?: number) => {
@@ -215,8 +255,8 @@ const open = async (type: string, id?: number) => {
       formLoading.value = false
     }
   }
-  companyList.value = await FinanceCompanyApi.getSimpleFinanceCompanyList()
-
+  const response = await FinanceCompanyApi.getSimpleFinanceCompanyList()
+  companyList.value = response.data
 }
 defineExpose({ open }) // 提供 open 方法，用于打开弹窗
 
@@ -248,6 +288,7 @@ const submitForm = async () => {
 const resetForm = () => {
   formData.value = {
     id: undefined,
+    applicationCode: undefined,
     userId: undefined,
     companyId: undefined,
     leasedProperty: undefined,
@@ -271,3 +312,81 @@ const resetForm = () => {
   formRef.value?.resetFields()
 }
 </script>
+
+<style scoped>
+/* 表单区域样式 */
+.form-section {
+  margin-bottom: 30px;
+}
+
+.section-title {
+  margin: 0 0 20px 0;
+  padding: 0 0 10px 0;
+  border-bottom: 2px solid #e4e7ed;
+  color: #303133;
+  font-size: 16px;
+  font-weight: 600;
+}
+
+/* 一行三列布局 */
+.form-row {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+
+.form-row .el-form-item {
+  flex: 1;
+  margin-bottom: 0;
+}
+
+/* Tab组件样式 */
+.form-tabs {
+  margin-top: 20px;
+}
+
+.radio-content,
+.upload-content {
+  padding: 20px 0;
+}
+
+/* 单选部分一行两列布局 */
+.radio-row {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+
+.radio-row .el-form-item {
+  flex: 1;
+  margin-bottom: 0;
+}
+
+/* 自定义对话框样式，占满除左侧导航栏以外的部分 */
+:deep(.right-full-dia) {
+  width: calc(100vw - 200px) !important;
+  max-width: none !important;
+  margin: 0 !important;
+  left: 200px !important;
+  top: 0 !important;
+  height: 100vh !important;
+  border-radius: 0 !important;
+}
+
+:deep(.right-full-dia .el-dialog__body) {
+  height: calc(100vh - 120px) !important;
+  overflow-y: auto;
+  padding: 20px;
+}
+
+:deep(.right-full-dia .el-dialog__header) {
+  padding: 20px 20px 0 20px;
+  border-bottom: 1px solid #e4e7ed;
+}
+
+:deep(.right-full-dia .el-dialog__footer) {
+  padding: 0 20px 20px 20px;
+  border-top: 1px solid #e4e7ed;
+  background: #fafafa;
+}
+</style>
