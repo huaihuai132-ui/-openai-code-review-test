e067bf9e1cc0fc62832ff3dc686181627b960735
insist_anan <1530885478@qq.com>
2025-09-09 16:31:46 +0800

    #36 feat 添加下载app的按钮

--- src/layout/components/ToolHeader.vue
+++ src/layout/components/ToolHeader.vue
@@ -1,5 +1,5 @@
 <script lang="tsx">
-import { defineComponent, computed } from 'vue'
+import { defineComponent, computed, ref } from 'vue'
 import { Message } from '@/layout/components//Message'
 import { Collapse } from '@/layout/components/Collapse'
 import { UserInfo } from '@/layout/components/UserInfo'
@@ -10,6 +10,8 @@ import { LocaleDropdown } from '@/layout/components/LocaleDropdown'
 import RouterSearch from '@/components/RouterSearch/index.vue'
 import { useAppStore } from '@/store/modules/app'
 import { useDesign } from '@/hooks/web/useDesign'
+import { ElTooltip } from 'element-plus'
+import appImage from '@/assets/app.png'
 
 const { getPrefixCls, variables } = useDesign()
 
@@ -45,6 +47,20 @@ const message = computed(() => appStore.getMessage)
 export default defineComponent({
   name: 'ToolHeader',
   setup() {
+    const showAppImage = ref(false)
+
+    const handleAppClick = () => {
+      showAppImage.value = !showAppImage.value
+    }
+
+    const handleAppMouseEnter = () => {
+      showAppImage.value = true
+    }
+
+    const handleAppMouseLeave = () => {
+      showAppImage.value = false
+    }
+
     return () => (
       <div
         id={`${variables.namespace}-tool-header`}
@@ -63,6 +79,31 @@ export default defineComponent({
           </div>
         ) : undefined}
         <div class="h-full flex items-center">
+          <div class="relative">
+            <div
+              class="custom-hover h-[var(--top-tool-height)] w-[var(--top-tool-height)] flex items-center justify-center cursor-pointer"
+              onClick={handleAppClick}
+              onMouseenter={handleAppMouseEnter}
+              onMouseleave={handleAppMouseLeave}
+            >
+              <svg
+                class="w-5 h-5"
+                fill="var(--top-header-text-color)"
+                viewBox="0 0 24 24"
+              >
+                <path d="M17.6,9.48l1.84-3.18c0.16-0.31,0.04-0.69-0.26-0.85c-0.29-0.15-0.65-0.06-0.83,0.22l-1.88,3.24 c-2.86-1.21-6.08-1.21-8.94,0L5.65,5.67c-0.19-0.29-0.58-0.38-0.87-0.22C4.5,5.65,4.41,6.01,4.56,6.3L6.4,9.48 C3.3,11.25,1.28,14.44,1,18h22C22.72,14.44,20.7,11.25,17.6,9.48z M7,15.25c-0.69,0-1.25-0.56-1.25-1.25 c0-0.69,0.56-1.25,1.25-1.25S8.25,13.31,8.25,14C8.25,14.69,7.69,15.25,7,15.25z M17,15.25c-0.69,0-1.25-0.56-1.25-1.25 c0-0.69,0.56-1.25,1.25-1.25s1.25,0.56,1.25,1.25C18.25,14.69,17.69,15.25,17,15.25z"/>
+              </svg>
+            </div>
+            {showAppImage.value && (
+              <div class="absolute top-full left-0 mt-2 z-50 bg-white rounded-lg shadow-lg border p-2">
+                <img
+                  src={appImage}
+                  alt="App"
+                  class="w-48 h-auto rounded"
+                />
+              </div>
+            )}
+          </div>
           {screenfull.value ? (
             <Screenfull class="custom-hover" color="var(--top-header-text-color)"></Screenfull>
           ) : undefined}
